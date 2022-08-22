65d0c47f4d2745d96f55ae9efb35a2ee9aacc760
suhasbr <suhas.bangalore16@gmail.com>
2022-08-22 23:14:30 +0530

    prod ver 4.1.3

--- ipfs/ipfs.js
+++ ipfs/ipfs.js
@@ -3,7 +3,6 @@ const {Web3Storage} = require('web3.storage');
 function getAccessToken() {
     // If you're just testing, you can paste in a token
     // and uncomment the following line:
-    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGFGRGVBMjdCMzBGRGE5OTlhRjdEMTlCZDNCYTE5ODM0NGViZTllQUQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjEwOTE5MzU2ODQsIm5hbWUiOiJUZWNobm9sb2d5REFPIn0.1f0ozGRMDzpk76j6XqaxFNToyYk3-f973IsHbtjZPfQ'
     // In a real app, it's better to read an access token from an
     // environement variable or other configuration that's kept outside of
     // your code base. For this to work, you need to set the
